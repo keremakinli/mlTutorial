889c88ed6472c6bd1858ab6fb3f2bc603653ae32
Vin_04 <vinayakborhade@gmail.com>
2019-03-20 21:48:22 +0530

    adding knn dataset and KNNClassifier.py

---
+++ KNNClassifier.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import sklearn.model_selection as ms
+import sklearn.neighbors as ng
+
+df=pd.read_csv('breast-cancer-wisconsin.data.txt')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+X=np.array(df.drop(['class'], 1) )
+y=np.array(df['class'])
+X_train, X_test, y_train, y_test=ms.train_test_split(X, y, test_size=0.2)
+sklearn.model_selection.train_test_split
+clf=ng.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+accuracy=clf.score(X_test, y_test)
+print(accuracy)
